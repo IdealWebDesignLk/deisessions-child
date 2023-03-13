aa81979d057befc5cc71f29c3933a2bf3ed95b2f
kamindu dushmantha <kamindudushmantha@gmail.com>
2023-03-13 15:52:06 +0530

    Added id attribute to carousel items

--- page-home-merged-new-latest.php
+++ page-home-merged-new-latest.php
@@ -365,6 +365,7 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
                     <input type="text" id="kd-price-to" onkeyup="selectResultBasedTitle(event)">
                 </div>
                 <div class="kd-form-group">
+                    <label>&nbsp;</label>
                     <button class="kd-reset-btn" onclick="resetSearch()">Reset Filters</button>
                 </div>
             </form>
@@ -457,7 +458,7 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
                                         ?>
 
 
-                                            <div class="item mainitem kd-service-slide" data-tags="<?php echo $tagone . $tagtwo . $tagthree . $tagfour . $tagfive; ?>" data-expert = "<?php echo $employeedetails->firstName . " " . $employeedetails->lastName;  ?>" data-category="<?php echo $catResult->name; ?>" data-price="<?php echo $row->price; ?>" data-name="<?php echo $row->name; ?>">
+                                            <div class="item mainitem kd-service-slide" data-id="<?php echo $servicesingleid; ?>" data-tags="<?php echo $tagone . $tagtwo . $tagthree . $tagfour . $tagfive; ?>" data-expert = "<?php echo $employeedetails->firstName . " " . $employeedetails->lastName;  ?>" data-category="<?php echo $catResult->name; ?>" data-price="<?php echo $row->price; ?>" data-name="<?php echo $row->name; ?>">
 
                                                 <div onmouseleave="kdAdddeactivatedThumb(event)" onmouseenter="kdOpenPopupFunc(event)" class="gallery-video-thumbnail kd-thumbnnail" data-id="<?php echo $servicesingleid; ?>">
                                                     <a data-id="<?php echo $servicesingleid; ?>">
