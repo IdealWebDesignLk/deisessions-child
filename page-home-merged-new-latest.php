6af8a5b2bdb2ff32fc2d951ddd9c3e43c8f99926
madara-95 <127185217+madara-95@users.noreply.github.com>
2023-03-14 10:40:47 +0530

    added class to the h2 tag

--- page-home-merged-new-latest.php
+++ page-home-merged-new-latest.php
@@ -343,7 +343,7 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
                         $categoriesSql = "SELECT * FROM $tbprefix" . "amelia_categories GROUP BY `id` ORDER BY `position`;";
                         $catResults = $wpdb->get_results($categoriesSql);
                                             ?>
-                                            <h2><?php the_field('category'); ?></h2>
+                                            <h2 class="cateids"><?php echo the_field('category'); ?></h2>
                         <?php
                         $exclude_cat_id = array(17, 23, 19, 12, 9, 8, 18, 4, 12, 44, 28, 29, 42, 41, 40);
                         foreach ($catResults as $catResult) {
