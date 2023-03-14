1856c83057c45182cf1e2e694152df040b3ad57f
madara-95 <127185217+madara-95@users.noreply.github.com>
2023-03-14 10:38:38 +0530

    re correct php tags

--- page-home-merged-new-latest.php
+++ page-home-merged-new-latest.php
@@ -342,7 +342,9 @@ $videosrc =  $server_name . '/wp-content/uploads/2022/09/pexels-artem-podrez-575
                         global $wpdb;
                         $categoriesSql = "SELECT * FROM $tbprefix" . "amelia_categories GROUP BY `id` ORDER BY `position`;";
                         $catResults = $wpdb->get_results($categoriesSql);
+                                            ?>
                                             <h2><?php the_field('category'); ?></h2>
+                        <?php
                         $exclude_cat_id = array(17, 23, 19, 12, 9, 8, 18, 4, 12, 44, 28, 29, 42, 41, 40);
                         foreach ($catResults as $catResult) {
                             if (!in_array(intval($catResult->id), $exclude_cat_id)) {
